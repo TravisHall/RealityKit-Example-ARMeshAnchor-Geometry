061e0effe0dd41d5203129cc1a19c5e723cea723
Travis Hall <travis.hall.design@gmail.com>
2021-11-24 20:46:13 +0000

    Initial Commit

---
+++ RealityKit Example ARMeshAnchor/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  RealityKit Example ARMeshAnchor
+//
+//  Created by Travis Hall on 24/11/2021.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ RealityKit Example ARMeshAnchorUITests/RealityKit_Example_ARMeshAnchorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RealityKit_Example_ARMeshAnchorUITestsLaunchTests.swift
+//  RealityKit Example ARMeshAnchorUITests
+//
+//  Created by Travis Hall on 24/11/2021.
+//
+
+import XCTest
+
+class RealityKit_Example_ARMeshAnchorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
